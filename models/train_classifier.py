1c9836cc34b39b54e1f7c21f47c37fef119c06b8
Luong Click <luongnguyentrongduc@gmail.com>
2024-04-28 23:11:44 +0700

    move code too train_classifier.py

--- models/train_classifier.py
+++ models/train_classifier.py
@@ -1,24 +1,103 @@
 import sys
 
+# import libraries
+import pandas as pd
+
+import nltk
+nltk.download('punkt')
+
+import pickle
+from sqlalchemy import create_engine
+from nltk.tokenize import word_tokenize
+from nltk.stem import WordNetLemmatizer
+from sklearn.metrics import confusion_matrix
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.pipeline import Pipeline, FeatureUnion
+from sklearn.base import BaseEstimator, TransformerMixin
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+
+from sklearn.multioutput import MultiOutputClassifier
+from sklearn.ensemble import GradientBoostingClassifier, RandomForestClassifier, AdaBoostClassifier
+from sklearn.metrics import classification_report
+
+class StartingVerbExtractor(BaseEstimator, TransformerMixin):
+
+    def starting_verb(self, text):
+        sentence_list = nltk.sent_tokenize(text)
+        for sentence in sentence_list:
+            pos_tags = nltk.pos_tag(tokenize(sentence))
+            first_word, first_tag = pos_tags[0]
+            if first_tag in ['VB', 'VBP'] or first_word == 'RT':
+                return True
+        return False
+
+    def fit(self, X, y=None):
+        return self
+
+    def transform(self, X):
+        X_tagged = pd.Series(X).apply(self.starting_verb)
+        return pd.DataFrame(X_tagged)
 
 def load_data(database_filepath):
-    pass
+    engine = create_engine(f'sqlite:///{database_filepath}')
+    df = pd.read_sql_table("clean_disaster_data", engine)
 
+    X = df["message"]
+    y = df.iloc[:, 4:]
+
+    column_names = y.columns.values
+
+    return X, y.values, column_names
 
 def tokenize(text):
-    pass
+    tokens = word_tokenize(text)
+    lemmatizer = WordNetLemmatizer()
 
+    clean_tokens = []
+    for tok in tokens:
+        clean_tok = lemmatizer.lemmatize(tok).lower().strip()
+        clean_tokens.append(clean_tok)
+
+    return clean_tokens
+    
 
 def build_model():
-    pass
+    # create pipeline
+    pipeline = Pipeline([
+        ('feature', FeatureUnion([
+            ('text_pipeline', Pipeline([
+                ('count_vectorizer', CountVectorizer(tokenizer=tokenize)),
+                ('tfidf_transformer', TfidfTransformer())
+            ])),
+            ('starting_verb', StartingVerbExtractor()) 
+        ])) ,
+        ('classifier', MultiOutputClassifier(AdaBoostClassifier()))
+    ])
+
+    # optimize hyperparameter with GridSearchCV
+    parameters =  {
+        'classifier__estimator__learning_rate': [0.5, 1.0],
+        'classifier__estimator__n_estimators': [10, 20]
+    }
+
+    cv = GridSearchCV(pipeline, param_grid=parameters, cv=5, n_jobs=-1, verbose=3) 
+
+    return cv
 
 
 def evaluate_model(model, X_test, Y_test, category_names):
-    pass
+    y_prediction = model.predict(X_test)
+
+    for idx, column in enumerate(category_names):
+        print("Target: ", column)
+        print(classification_report(Y_test[:, column], y_prediction[:, idx]))
+        print("---" * 10)
 
 
 def save_model(model, model_filepath):
-    pass
+    with open(model_filepath, 'wb') as f:
+        pickle.dump(model, f)
 
 
 def main():
@@ -27,6 +106,8 @@ def main():
         print('Loading data...\n    DATABASE: {}'.format(database_filepath))
         X, Y, category_names = load_data(database_filepath)
         X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2)
+
+        print(Y_test)
         
         print('Building model...')
         model = build_model()
